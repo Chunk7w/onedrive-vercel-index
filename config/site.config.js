4e2137ccb6c4f21bfcb367ff73d43afd457273d6
Chunk7 <59020457+Chunk7w@users.noreply.github.com>
2022-07-07 22:03:01 +0800

    add protectedRoutes

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,8 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/🧩 Others/🥚 真正的和平模式 - 彩蛋',
+  ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:chunk7w@outlook.com',
